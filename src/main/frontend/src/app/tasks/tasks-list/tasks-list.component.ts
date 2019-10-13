1fb8c0dcbfcb2b2b2e48c7959944e588d9ca99ac
mohanad010050 <mohanad2021996@gmail.com>
2019-10-13 19:00:50 +0200

    Init Tasks and add event handler

--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Task} from "../task,model";
 
 @Component({
   selector: 'app-tasks-list',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksListComponent implements OnInit {
 
-  constructor() { }
+  tasks: Task[] = [];
 
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.tasks.push(new Task(1, "task1", false, "07/08/2019"))
+    this.tasks.push(new Task(2, "task2", true, "07/08/2020"))
+    this.tasks.push(new Task(3, "task3", true, "07/08/2021"))
+  }
+
+  onTaskChange($event: Event, task: Task) {
+    console.log(task.name +" changed ,"+task.completed)
+  }
+
+  getDueDateLabel(task: Task) {
+    return task.completed ? 'label-success' : 'label-primary';
+  }
 }
