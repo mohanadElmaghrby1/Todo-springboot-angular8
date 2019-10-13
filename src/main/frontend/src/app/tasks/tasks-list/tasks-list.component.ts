2ddfc09451be4a731d4576f59ec13b3550e96f71
mohanad010050 <mohanad2021996@gmail.com>
2019-10-13 20:44:11 +0200

    Subscribe To task-add EventEmitter

--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Task} from "../task,model";
+import {TaskService} from "../task.service";
 
 @Component({
   selector: 'app-tasks-list',
@@ -10,17 +11,28 @@ export class TasksListComponent implements OnInit {
 
   tasks: Task[] = [];
 
-  constructor() {
+  constructor(private taskService: TaskService) {
   }
 
   ngOnInit() {
-    this.tasks.push(new Task(1, "task1", false, "07/08/2019"))
-    this.tasks.push(new Task(2, "task2", true, "07/08/2020"))
-    this.tasks.push(new Task(3, "task3", true, "07/08/2021"))
+    this.taskService.getTasks().subscribe(
+      (tasks: any[]) => {
+        this.tasks=tasks;
+      },
+      (error) => console.log(error)
+    );
+    // this.tasks.push(new Task(1, "task1", false, "07/08/2019"))
+    // this.tasks.push(new Task(2, "task2", true, "07/08/2020"))
+    // this.tasks.push(new Task(3, "task3", true, "07/08/2021"))
+    //subscribe onTaskAdded Emitter
+    this.taskService.onTaskAdded.subscribe(
+      (task : Task)=>this.tasks.push(task)
+    );
   }
 
-  onTaskChange($event: Event, task: Task) {
-    console.log(task.name +" changed ,"+task.completed)
+  onTaskChange(event , task) {
+    this.taskService.saveTask(task , event.target.checked).subscribe();
+    console.log(task.name + " changed ," + task.completed)
   }
 
   getDueDateLabel(task: Task) {
