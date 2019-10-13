6e56da8eb79a7dbac3e072f8fcd7dbea55b0ba35
mohanad010050 <mohanad2021996@gmail.com>
2019-10-13 20:44:55 +0200

    add FormsModule

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { TasksAddComponent } from './tasks/tasks-add/tasks-add.component';
 import { TasksListComponent } from './tasks/tasks-list/tasks-list.component';
 import {TaskService} from "./tasks/task.service";
 import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import {HttpClientModule} from "@angular/common/http";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [TaskService],
   bootstrap: [AppComponent]
