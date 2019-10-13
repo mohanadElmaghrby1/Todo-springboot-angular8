f894f267344395ae698deecf7b16b10392fd586b
mohanad010050 <mohanad2021996@gmail.com>
2019-10-13 20:43:00 +0200

    Add EventEmitter for connect tass-add to task0list

--- src/main/frontend/src/app/tasks/task.service.ts
+++ src/main/frontend/src/app/tasks/task.service.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
-import {Injectable} from "@angular/core";
+import {EventEmitter, Injectable} from "@angular/core";
 import {Task} from "./task,model";
 
 @Injectable()
 export class TaskService {
-  constructor(private http:HttpClient){
 
+  onTaskAdded = new EventEmitter<Task>()
+
+  constructor(private http:HttpClient){
   }
 
   getTasks( ){
@@ -14,6 +16,11 @@ export class TaskService {
 
   saveTask(task :Task , completed : boolean){
     task.completed=completed;
+    return this.http.post('/api/tasks/save' , task);
+  }
+
+  addTask(task :Task){
+    console.log("create new task " +task.name );
     return this.http.post('/api/tasks/save' , task);
   }
 }
